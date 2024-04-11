2c35a36156777abb05fe7be4bbaebc4a5fb4c3d4
adonisamarante <adonisamarante@gmail.com>
2024-04-11 17:52:55 -0300

    feat: input component styles

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,8 +1,16 @@
+import styles from "./CustomInput.module.css"
+
 interface CustomInputProps {
   h?: number
   w?: number
 }
 
-export function CustomInput({ h = 10, w = 30 }: CustomInputProps) {
-  return <input style={{ height: h, width: w }} />
+export function CustomInput({ h = 54, w = 491 }: CustomInputProps) {
+  return (
+    <input
+      className={styles.customInput}
+      style={{ height: h, width: w }}
+      placeholder="Adicione uma nova tarefa"
+    />
+  )
 }
