4ce29e88ca36747177379807960cfa0805eafae6
adonisamarante <adonisamarante@gmail.com>
2024-05-07 11:03:50 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Task } from "./components/Task"
+import { Header } from "./components/Header"
 
 import "./App.module.css"
 import "./global.css"
@@ -6,7 +6,7 @@ import "./global.css"
 function App() {
   return (
     <div>
-      <Task />
+      <Header />
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
+import styles from "./Header.module.css"
+
+import TodoLogo from "./../assets/todo-logo.svg"
+
 export function Header() {
-  return <div><span style={{color: 'white+'}}>test</span></div>
+  return (
+    <header className={styles.header}>
+      <img src={TodoLogo} alt="Logotipo Todo App" />
+    </header>
+  )
 }
