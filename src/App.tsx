3501a55ba85b8d225975e134255775110bec4f8c
adonisamarante <adonisamarante@gmail.com>
2024-05-07 12:05:19 -0300

    feat: content wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Header } from "./components/Header"
-
-import "./App.module.css"
 import "./global.css"
+import styles from "./App.module.css"
+import { Header } from "./components/Header"
+import { CustomInput } from "./components/CustomInput"
+import { ButtonCreate } from "./components/ButtonCreate"
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.Wrapper}>
+        <div className={styles.CreateContainer}>
+          <CustomInput />
+          <ButtonCreate />
+        </div>
+      </div>
     </div>
   )
 }
