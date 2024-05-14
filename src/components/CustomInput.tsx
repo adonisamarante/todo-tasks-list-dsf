4c9acf9a0e47e11693afeecf0ca1a5619b356f0d
adonisamarante <adonisamarante@gmail.com>
2024-05-14 14:38:36 -0300

    feat: input logic

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,15 +1,29 @@
+import { ChangeEvent, InvalidEvent } from "react"
 import styles from "./CustomInput.module.css"
 
 interface CustomInputProps {
   w?: number | string
+  value?: string
+  onChangeDescription: (event: ChangeEvent<HTMLInputElement>) => void
+  onInvalid: (event: InvalidEvent<HTMLInputElement>) => void
 }
 
-export function CustomInput({ w = "inherit" }: CustomInputProps) {
+export function CustomInput({
+  w = "inherit",
+  value = "",
+  onChangeDescription,
+  onInvalid,
+}: CustomInputProps) {
   return (
     <input
+      name="description"
+      value={value}
       className={styles.customInput}
       style={{ width: w }}
+      onChange={onChangeDescription}
+      onInvalid={onInvalid}
       placeholder="Adicione uma nova tarefa"
+      required
     />
   )
 }
